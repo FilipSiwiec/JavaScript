6abb370495f374f3a7d470665120890a6f9d60a1
Filip Siwiec <filipsiwiec@gmal.com>
2019-09-24 17:10:30 +0200

    lesson 13 JS

--- test.js
+++ test.js
@@ -1,12 +1,25 @@
-var youLikeMeat = false;
-var myNum = 10;
-if (youLikeMeat){
+var myAge = 2;
 
-    document.write("I like meat");
+if (myAge > 30) {
+
+    document.write("You are over 30!");
 
 }
 
-else{
+else if (myAge > 20) {
 
-    document.write("i only eat greens");
+    document.write("You are over 20!");
+
+}
+
+else if (myAge > 10) {
+
+    document.write("You are over 10!");
+
+}
+
+else {
+
+    document.write("You are not over 10!");
+
 }
