167b655eed3ddd84d41939d9178d52a50d8efdcf
Filip Siwiec <filipsiwiec@gmal.com>
2019-09-24 23:29:56 +0200

    lesson 28 JS

--- test.js
+++ test.js
@@ -1,4 +1,27 @@
-var myString1 = "abc"
-var myString2 = "BCD"
+var myArray = new Array();
+myArray[0] = 8;
+myArray[2] = "hello";
 
-console.log( myString1 < myString2.toLowerCase);
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Shaun";
+
+myCar.drive = function(){
+console.log("now driving");
+};
+
+myCar.drive();
+
+
+var myCar2 = {
+
+    maxSpeed = 70,
+    driver: "Net Ninja",
+    drive: function(speed, time){
+        console.log(speed * time);
+    }
+
+};
+
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
