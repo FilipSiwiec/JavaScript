adb621437d3842b04769aac1aab27c717030e421
Filip Siwiec <filipsiwiec@gmal.com>
2019-09-25 03:03:19 +0200

    lesson 30 JS

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
-var myCar2 = {
+var Car = function(maxSpeed, driver){
 
-
-    maxSpeed: 70,
-    driver: "Net Ninja",
-    drive: function(speed, time){
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
         console.log(speed * time);
-    },
-
-    logDriver: function(){
+    };
+    this.logDriver = function(){
         console.log("driver name is " + this.driver);
-    }
-};
+    };
 
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+}
+
+var myCar = new Car (70, "ninja Man");
+var myCar2 = new Car (40, "Stefan");
+var myCar3 = new Car (10, "Andrzej");
+var myCar4 = new Car (90, "Mati");
+
+myCar.drive(30,5);
+myCar3.logDriver();
